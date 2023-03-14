6a1b983f1d0fc9557902dd68c05e35d5c1fd650e
DiegoLafeuille <diego.lafeuille@gmail.com>
2023-03-14 23:20:43 +0100

    Added ROIs selection

--- main.py
+++ main.py
@@ -1,67 +1,87 @@
-from PIL import Image
+import cv2
 from pytesseract import pytesseract
 
 
-# # If you don't have tesseract executable in your PATH, include the following:
+def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    return cv2.resize(image, dim, interpolation=inter)
+
+
+
 pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
-# # Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
+
+# Load image
+original_img = cv2.imread('images/cvt_spectrometer.jpg')
+original_img = ResizeWithAspectRatio(original_img, height= 720)
+
+# Apply contrast enhancement
+img = cv2.cvtColor(original_img, cv2.COLOR_BGR2GRAY)
+img = cv2.equalizeHist(img)
+
+# Apply noise reduction
+# img = cv2.medianBlur(img, 3)
+
+# Apply adaptive thresholding
+img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 10)
+
+# Define ROI
+rois = cv2.selectROIs("Select ROIs", original_img)
+cv2.destroyWindow("Select ROIs")
+print(rois)
+
+# Apply OCR
+for roi in rois:
+    roi_img = img[roi[0]:roi[0]+roi[2], roi[1]:roi[1]+roi[3]]
+    roi_text = pytesseract.image_to_string(roi_img, config='--psm 11')
+
+    if roi_text == '':
+        roi_text = 'No text'
+
+    # Draw bounding boxes around recognized text
+    x = roi[0]
+    y = roi[1]
+    w = roi[2]
+    h = roi[3]
+    cv2.rectangle(original_img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+    cv2.putText(original_img, roi_text, (x, y - 16), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
 
 
-# # Simple image to string
-print(pytesseract.image_to_string(Image.open('images/cvt_spectrometer.jpg'), config='--psm 11'))
+# Display image
+winname = 'Original image'
+cv2.namedWindow(winname)
+cv2.moveWindow(winname, 40,30)
+cv2.imshow(winname, original_img)
+cv2.waitKey(0)
 
-# # In order to bypass the image conversions of pytesseract, just use relative or absolute image path
-# # NOTE: In this case you should provide tesseract supported images or tesseract will return error
-# print(pytesseract.image_to_string('images/cvt_spectrometer.jpg'))
+img = ResizeWithAspectRatio(img, height= 720)
+winname = 'Processed image'
+cv2.namedWindow(winname)
+cv2.moveWindow(winname, 40,30)
+cv2.imshow(winname, img)
+cv2.waitKey(0)
 
-# # List of available languages
-# print(pytesseract.get_languages(config=''))
+# gray = ResizeWithAspectRatio(gray, height= 720)
+# winname = 'Gray'
+# cv2.namedWindow(winname)
+# cv2.moveWindow(winname, 40,30)
+# cv2.imshow(winname, gray)
+# cv2.waitKey(0)
 
-# # French text image to string
-# print(pytesseract.image_to_string(Image.open('test-european.jpg'), lang='fra'))
+# thresh = ResizeWithAspectRatio(thresh, height= 720)
+# winname = 'Thresh'
+# cv2.namedWindow(winname)
+# cv2.moveWindow(winname, 40,30)
+# cv2.imshow(winname, thresh)
+# cv2.waitKey(0)
 
-# # Batch processing with a single file containing the list of multiple image file paths
-# print(pytesseract.image_to_string('images.txt'))
-
-# # Timeout/terminate the tesseract job after a period of time
-# try:
-#     print(pytesseract.image_to_string('test.jpg', timeout=2)) # Timeout after 2 seconds
-#     print(pytesseract.image_to_string('test.jpg', timeout=0.5)) # Timeout after half a second
-# except RuntimeError as timeout_error:
-#     # Tesseract processing is terminated
-#     pass
-
-# # Get bounding box estimates
-# print(pytesseract.image_to_boxes(Image.open('images/cvt_spectrometer.jpg')))
-
-# # Get verbose data including boxes, confidences, line and page numbers
-# print(pytesseract.image_to_data(Image.open('images/cvt_spectrometer.jpg')))
-
-# # Get information about orientation and script detection
-# print(pytesseract.image_to_osd(Image.open('images/cvt_spectrometer.jpg')))
-
-# # Get a searchable PDF
-# pdf = pytesseract.image_to_pdf_or_hocr('images/cvt_spectrometer.jpg', extension='pdf')
-# with open('test.pdf', 'w+b') as f:
-#     f.write(pdf) # pdf type is bytes by default
-
-# # Get HOCR output
-# hocr = pytesseract.image_to_pdf_or_hocr('images/cvt_spectrometer.jpg', extension='hocr')
-
-# # Get ALTO XML output
-# xml = pytesseract.image_to_alto_xml('images/cvt_spectrometer.jpg')
-
-
-
-# import cv2
-
-
-# img_cv = cv2.imread(r'images/cvt_spectrometer.jpg')
-
-# # By default OpenCV stores images in BGR format and since pytesseract assumes RGB format,
-# # we need to convert from BGR to RGB format/mode:
-# # img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
-# # print(pytesseract.image_to_string(img_rgb))
-# # OR
-# # img_rgb = Image.frombytes('RGB', img_cv.shape[:2], img_cv, 'raw', 'BGR', 0, 0)
-# print(pytesseract.image_to_string(img_cv))
