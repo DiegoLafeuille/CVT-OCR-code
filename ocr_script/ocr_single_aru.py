e44276ca43185bb3985cf6d2a6d91a06bec6665b
Diego Lafeuille <d.lafeuille@colvistec.de>
2023-04-21 15:21:14 +0200

    Progress on single marker method

--- ocr_script/ocr_single_aru.py
+++ ocr_script/ocr_single_aru.py
@@ -1,31 +1,74 @@
 import cv2
 import numpy as np
+import pickle
 
-# Load the image and camera parameters
-img = cv2.imread('image.jpg')
-camera_matrix = np.loadtxt('camera_matrix.txt')
-dist_coeffs = np.loadtxt('dist_coeffs.txt')
 
-# Define the ArUco dictionary and parameters
-aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
-aruco_params = cv2.aruco.DetectorParameters_create()
 
-# Detect the ArUco marker in the image
-corners, ids, _ = cv2.aruco.detectMarkers(img, aruco_dict, parameters=aruco_params)
+def update_cam(camera):
+    print(f"Updating camera: {camera}")
+    # Path to pickle file
+    calib_file_path = "cam_calibration/cameras/" + camera + "/calibration_params.pickle"
 
-if ids is not None:
-    # Estimate the pose of the marker
-    rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(corners, 0.05, camera_matrix, dist_coeffs)
+    # Load the calibration parameters from the pickle file
+    with open(calib_file_path, 'rb') as f:
+        calibration_params = pickle.load(f)
 
-    # Define the 3D points of the rectangle
-    rect_points_3d = np.array([[0, 0, 0], [0, 1, 0], [1, 1, 0], [1, 0, 0]], dtype=np.float32)
+    # Extract the parameters from the dictionary
+    mtx = calibration_params["mtx"]
+    dist = calibration_params["dist"]
+    calib_w = int(calibration_params["calib_w"])
+    calib_h = int(calibration_params["calib_h"])
+    print(f"{calib_w}x{calib_h}")
 
-    # Project the 3D points onto the 2D image plane using the camera parameters
-    rect_points_2d, _ = cv2.projectPoints(rect_points_3d, rvecs, tvecs, camera_matrix, dist_coeffs)
+    if any(x is None for x in (mtx, dist)):
+        print("Failed to retrieve calibration parameters.")
+        return None
+    
+    return mtx, dist, calib_w, calib_h
 
-    # Draw the detected rectangle on the image
-    img = cv2.polylines(img, [np.int32(rect_points_2d)], True, (0, 255, 0), 2)
 
-cv2.imshow('Image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def main():
+    # Load the image and camera parameters
+    img = cv2.imread('c:/Users/DLafeuille/Desktop/BachelorArbeit/CVT-OCR-code/cam_calibration/cameras/jans_webcam/WIN_20230414_09_38_46_Pro.jpg')
+    camera_matrix, dist_coeffs, _, _ = update_cam("jans_webcam")
+
+    # Define the ArUco dictionary and parameters
+    aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_50)
+    marker_size = 0.08
+
+    # Detect the ArUco marker in the image
+    corners, ids, _ = cv2.aruco.detectMarkers(img, aruco_dict,)
+
+    cv2.aruco.drawDetectedMarkers(img, corners) 
+
+    if ids is not None:
+        id1 = np.where(ids == 1)[0][0]
+        
+        # Estimate the pose of the marker
+        rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners[id1], marker_size, camera_matrix, dist_coeffs)
+
+        cv2.drawFrameAxes(img, camera_matrix, dist_coeffs, rvec, tvec, 0.1, 3)
+
+        # Calculate the 3D points of the rectangle based on width, height, corner coordinates, and rotation angle
+        width = 0.297  # width of rectangle in meters
+        height = 0.210  # height of rectangle in meters
+        x = marker_size/2  # x coordinate of upper left corner of rectangle in meters
+        y = -marker_size/2  # y coordinate of upper left corner of rectangle in meters
+        angle = 0  # rotation angle of rectangle in degrees
+        rect_points_3d = np.array([[x, y, 0], [x + width*np.cos(np.deg2rad(angle)), y + width*np.sin(np.deg2rad(angle)), 0], [x + height*np.sin(np.deg2rad(angle)) + width*np.cos(np.deg2rad(angle)), y - height*np.cos(np.deg2rad(angle)) + width*np.sin(np.deg2rad(angle)), 0], [x + height*np.sin(np.deg2rad(angle)), y - height*np.cos(np.deg2rad(angle)), 0]], dtype=np.float32)
+
+
+        # Project the 3D points onto the 2D image plane using the camera parameters
+        rect_points_2d, _ = cv2.projectPoints(rect_points_3d, rvec, tvec, camera_matrix, dist_coeffs)
+
+        # Draw the detected rectangle on the image
+        img = cv2.polylines(img, [np.int32(rect_points_2d)], True, (0, 255, 0), 2)
+
+    img = cv2.resize(img, (int(1920/2),int(1080/2)))
+
+    cv2.imshow('Image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == main():
+    main()
